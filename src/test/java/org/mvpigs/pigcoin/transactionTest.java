691e40b411aa24b097c60112868d4e07f9e72f57
David <gelpiorama@gmail.com>
2018-02-02 19:25:22 +0100

    inicio proyecto con primera clase test

---
+++ src/test/java/org/mvpigs/pigcoin/transactionTest.java
@@ -0,0 +1,16 @@
+package org.mvpigs.pigcoin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class transactionTest {
+
+    @Test
+    public void constructor_test() {
+        assertTrue(false);
+    }
+
+    
+
+}
