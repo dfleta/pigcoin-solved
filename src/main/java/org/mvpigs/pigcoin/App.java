7a59ae2ea193cd58fa172b3ccd87eb0cfc680bd8
David <gelpiorama@gmail.com>
2018-02-02 20:32:51 +0100

    crear las dos transacciones origen que crean moneda

--- src/main/java/org/mvpigs/pigcoin/App.java
+++ src/main/java/org/mvpigs/pigcoin/App.java
@@ -16,14 +16,18 @@ public class App
                                 "==============="        );
 
         Transaction transaction = new Transaction();
-        transaction = new Transaction("hash_1", "0", "origin", "wallet_1", 20);
+        transaction = new Transaction("hash_1", "0", "aaaaaaaa", "0", 20);
         transaction.sumarize();
 
         System.out.print("\n" + "Ver BlockChain" + "\n" + 
                                 "=============="        );
 
         BlockChain bChain = new BlockChain();
+        transaction = new Transaction("hash_1", "0", "origin", "wallet_1", 20);
         bChain.addOrigin(transaction);
+        transaction = new Transaction("hash_2", "1", "origin", "wallet_2", 10);
+        bChain.addOrigin(transaction);
+
         bChain.sumarize();
 
         System.out.print("\n" + "Ver Transaccion en posicion 0 del BlockChain" + "\n" + 
