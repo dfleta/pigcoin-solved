14403b865f76f62aaccd5428ae4ef68766f574a9
David <gelpiorama@gmail.com>
2018-02-09 20:24:49 +0100

    limpieza de imports sin usar

--- src/main/java/org/mvpigs/pigcoin/BlockChain.java
+++ src/main/java/org/mvpigs/pigcoin/BlockChain.java
@@ -2,11 +2,8 @@ package org.mvpigs.pigcoin;
 
 import java.security.PublicKey;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class BlockChain {
